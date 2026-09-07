0322206b36f89e984d717ccb18e4f9e311d8ea85
Theodor Gyllner <thgy1740@student.su.se>
2026-09-07 10:14:51 +0200

    updating api

--- src/api.py
+++ src/api.py
@@ -10,5 +10,5 @@ def hello():
 # making sure the docker application not only listens on 127.0.0.1 which means it only listens inside of the container.
 # Using 0.0.0.0 allows traffic to reach flask
 if __name__ == "__main__": 
-    app.run(host="0.0.0.0", port=5000) #this port needs to be specified in the dockerfile also.
+    app.run(host="0.0.0.0", port=5000) #this port needs to be specified in the dockerfile also. 5000 is often used for hosting a docker registry, flask or python dev server.
     
