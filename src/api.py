381bc9b0cb1962e1e8dbdafac48bce18cd67ee14
Theodor Gyllner <thgy1740@student.su.se>
2026-09-07 10:03:13 +0200

    test

--- src/api.py
+++ src/api.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 @app.route("/hello", methods=["GET"])
 def hello():
     return jsonify({"message": "Hello world!"})
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
