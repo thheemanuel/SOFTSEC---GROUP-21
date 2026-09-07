cd7b2de5f5eb8c2f01d8471fc48d78f740299eea
Theodor Gyllner <thgy1740@student.su.se>
2026-09-07 10:11:46 +0200

    updating api

--- src/api.py
+++ src/api.py
@@ -7,5 +7,8 @@ app = Flask(__name__)
 def hello():
     return jsonify({"message": "Hello world!"})
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# making sure the docker application not only listens on 127.0.0.1 which means it only listens inside of the container.
+# Using 0.0.0.0 allows traffic to reach flask
+if __name__ == "__main__": 
+    app.run(host="0.0.0.0", port=5000) 
+    
